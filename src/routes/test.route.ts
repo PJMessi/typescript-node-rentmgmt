7809e7ba47df8b476ef8beff630bcfab2501a817
Prajwal Shrestha <prajwal.shrestha@hazesoft.co>
2021-03-27 10:49:54 +0545

    :wrench: Created new tsconfig for build

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -1,17 +1,6 @@
 import { NextFunction, Request, Response, Router } from 'express';
-import axios from 'axios';
 
 const router = Router();
-
-const testFunction1 = () => {
-  axios('https://jsonplaceholder.typicode.com/todos/1')
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
 
 const testFunction = async (
   req: Request,
@@ -19,9 +8,6 @@ const testFunction = async (
   next: NextFunction
 ): Promise<void | Response> => {
   try {
-    console.log('before');
-    testFunction1();
-    console.log('after');
     return res.json({
       success: true,
     });
